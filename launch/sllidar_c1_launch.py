bf62144b09e3fec1ec1e81f2720bbe2f58197aaf
jetson <miyao03023@gmail.com>
2024-11-12 22:40:57 +0900

    fix remapping

--- launch/sllidar_c1_launch.py
+++ launch/sllidar_c1_launch.py
@@ -18,7 +18,7 @@ def generate_launch_description():
     inverted = LaunchConfiguration('inverted', default='false')
     angle_compensate = LaunchConfiguration('angle_compensate', default='true')
     scan_mode = LaunchConfiguration('scan_mode', default='Standard')
-    topic_name = LaunchConfiguration('scan_topic', default='/scan')
+    topic_name = LaunchConfiguration('topic_name', default='/scan')
 
     return LaunchDescription([
         DeclareLaunchArgument(
